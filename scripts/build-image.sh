a1258d2a843210fe40e7f9f3bfadde85921fd17f
Dean Ayalon <deanayalon@me.com>
2024-03-12 01:52:26 +0000

    Added options and help dialog to the shell scripts

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-# WARN - Run from the context of the verdaccio repository's root directory!
 
-NO_CACHE=$1
+# Flags
+version="latest" # Default version
+path="."
+while getopts "hcv:" opt; do
+    case $opt in
+        h)
+            echo "Use: build-image.sh [-flag]"
+            echo "  Flags:"
+            echo "  -c(ache)    Build the image WITHOUT using cache                         Default: false"
+            echo "  -p(ath)     Specify path to the directory containing the dockerfile.    Default: ."
+            echo "  -v(ersion)  Specify version tag.                                        Default: latest"
+            echo "  -h(elp)     Display this help dialog"
+            exit 1 ;;
+        c)
+            no_cache=--no-cache ;;
+        p)  
+            path=$OPTARG ;;
+        v)
+            version=$OPTARG ;;
+        \?) 
+            exit 1 ;;
+    esac
+done
+shift $((OPTIND-1))
+flags=$no_cache
 
-# docker build -t certs . -f dockerfile.certs
-if [ "$NO_CACHE" = "1" ]; then
-    docker build --no-cache -t dean-verdaccio .
-else
-    docker build -t dean-verdaccio .
-fi
+# docker build $no_cache -t certs . -f dockerfile.certs
+docker build $flags -t dean-verdaccio:$version $path
--- scripts/new-container.sh
+++ scripts/new-container.sh
@@ -1,16 +1,36 @@
 #!/bin/bash 
 
-NAME=$1
-# PORT=$2
+version="latest" # Default version
+
+# Flags
+port=4873
+while getopts "hpv:" opt; do
+    case $opt in
+        h)
+            echo Use: build-image.sh [-flags] [container name]
+            echo "  Flags:"
+            echo "  -p(ath)     Specify host port to map to Verdaccio.  Default: 4873"
+            echo "  -v(ersion)  Specify version tag.                    Default: latest"
+            echo "  -h(elp)     Display this help dialog"
+            exit 1
+            ;;
+        p)
+            port=$OPTARG ;;
+        v)
+            version=$OPTARG ;;
+        \?) 
+            exit 1 ;;
+    esac
+done
+shift $((OPTIND-1))
+
 
 IMAGE=dean-verdaccio
+name=$1
 
-if [ -z $NAME ]; then
-    NAME=$IMAGE
+if [ -z $name ]; then
+    name=$IMAGE
 fi
-# if [ -z $PORT ]; then
-#     PORT=4873
-# fi
 
-docker run -dit --name=$NAME -p 4873:4873 $IMAGE
-# docker run -dit --name=$NAME -p $PORT:4873 $IMAGE
+# docker run -dit --name=$name -p 4873:4873 $IMAGE
+docker run -dit --name=$name -p $PORT:4873 $IMAGE:$version
