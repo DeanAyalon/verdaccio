bf92e292ae68900942816549e9691bc0c2b4580a
Dean Ayalon <deanayalon@me.com>
2024-03-15 13:13:31 +0000

    certificates script now allows defining a path for the generation new-container script now allows using the -m flag to define a mounts path

--- scripts/new-container.sh
+++ scripts/new-container.sh
@@ -1,21 +1,32 @@
 #!/bin/bash 
 
-# Flags
+scripts=$(dirname $(readlink -f $0))
+
+# Defaults
+mounts=~/Documents/verdaccio
+port=4873
+version="latest" 
+# Help
 help() {
     echo Use: new-container.sh [-flags] [container name]
     echo "  Default container name: dean-verdaccio"
     echo "  Flags:"
-    echo "  -p(ort)     Specify host port to map to Verdaccio.  Default: 4873"
-    echo "  -v(ersion)  Specify version tag.                    Default: latest"
+    echo "  -m(ounts)   Specify path to mount a volume for persistant storage                               Default: $mount"
+    echo "              The mounted directory should contain a certs folder with SSL certificates:"
+    echo "                  verdaccio-key.pem, verdaccio-csr.pem, verdaccio-cert.pem"
+    echo "              Self-signed certificates can be created using $scripts/certificates.sh"
+    # echo "              Use '!path' to avoid mounting a volume, instead specify a path for the certificate"
+    echo "  -p(ort)     Specify host port to map to Verdaccio.                                              Default: $port"
+    echo "  -v(ersion)  Specify version tag.                                                                Default: $version"
     echo "  -h(elp)     Display this help dialog"
 }
-
-version="latest" # Default version
-port=4873
-while getopts "hp:v:" opt; do
+# Flags
+while getopts "hm:p:v:" opt; do
     case $opt in
         h)
             help; exit 1 ;;
+        m)
+            mounts="$OPTARG" ;;
         p)
             port=$OPTARG ;;
         v)
@@ -26,19 +37,23 @@ while getopts "hp:v:" opt; do
 done
 shift $((OPTIND-1))
 
+if [ "$mounts" = "!" ]; then 
+    mountcmd=""
+else
+    mountcmd="-v $mounts/storage:/verdaccio/storage -v $mounts/certs:/verdaccio/certs"
+fi
+
 
 IMAGE=dean-verdaccio
 name=$1
 
 [ -z $name ] && name=$IMAGE
 
-mounts=~/Documents/verdaccio
+cmd="docker run -dit --name=$name -p $port:4873 $mountcmd $IMAGE:$version"
 
 echo Running new Docker Container $name using Image $IMAGE
 echo "Port: $port   Version: $version"
+[ -z "$mountcmd" ] || echo "Mount: $mount"
 echo
-echo "> docker run -dit --name=$name -p $port:4873 $IMAGE:$version"
-docker run -dit --name=$name -p $port:4873  \
-    -v $mounts/storage:/verdaccio/storage   \
-    -v $mounts/certs:/verdaccio/certs       \
-    $IMAGE:$version 
+echo "> $cmd"
+$cmd
