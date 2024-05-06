a010b3f0c057978bfe6587aa13b90e0fb45903d6
Dean Ayalon <deanayalon@me.com>
2024-05-06 03:31:32 +0300

    Healthchecks verifies .env is up to date

--- files/scripts/healthcheck.sh
+++ files/scripts/healthcheck.sh
@@ -1,20 +1,40 @@
 #!/bin/sh
 
-req="curl -f https://$DOMAIN:$PROXY_PORT"
+check_env() {
+    echo Checking environment
+    # VERDACCIO_ENV is a container environment variable
+    ([ -z $VERDACCIO_ENV ] || [ -z $LATEST_ENV ] || [ $LATEST_ENV != $VERDACCIO_ENV ]) && exit 1
+}
 
-echo "> $req"
-temp_file=$(mktemp)
-$req > "$temp_file" || exit 1
+check_site() {
+    echo Checking webserver
 
-echo Server is up, checking src
-# Check if the grep works
-# Check grep does not collide with packages having 'localhost' in their title
-host_err=$(grep "localhost" "$temp_file")
-rm $temp_file
+    req="curl -f https://$DOMAIN:$PROXY_PORT"
 
-if [ ! -z "$host_err" ]; then
-    echo "Script src incorrectly set to localhost"
-    exit 1
-else
-    echo "Container is healthy"
-fi
+    echo "> $req"
+    temp_file=$(mktemp)
+    $req > "$temp_file" || exit 1
+
+    echo Server is up, checking src
+    echo This check needs updating....
+
+    # TODO: IMPLEMENT BETTER CHECKING, AND MAKE SURE THE CORRECT URL IS IN SCRIPTS
+    # host_err=$(grep "localhost" "$temp_file")
+    # rm $temp_file
+
+    # if [ ! -z "$host_err" ]; then
+    #     echo "Script src incorrectly set to localhost"
+    #     exit 1
+    # else
+    #     echo "Site is responsive"
+    #     exit 0
+    # fi
+}
+
+main() {
+    echo Starting Verdaccio healthcheck
+    check_env
+    check_site
+}
+
+main
